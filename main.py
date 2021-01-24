bfb49a3f41d367585475254b34b670ec0752fa74
lrcilento <lucascilento@protonmail.com>
2021-01-24 16:03:37 -0300

    removed unused imports

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import asyncio
 import time
 import datetime
-import sys
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
