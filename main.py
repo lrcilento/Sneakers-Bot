bd49f5e26db90bcf2d41d7147c79b2f01fc0f974
lrcilento <lucascilento@protonmail.com>
2021-01-25 16:07:12 -0300

    automated geckodriver installation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import asyncio
 import time
 import datetime
+import geckodriver_autoinstaller
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
