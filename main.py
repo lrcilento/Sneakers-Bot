cdea85c1b39d3151835c2ee92c12e13234ca64ee
lrcilento <lucascilento@protonmail.com>
2021-01-25 17:18:51 -0300

    removed gecko-autoinstaller

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import asyncio
 import time
 import datetime
-import geckodriver_autoinstaller
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
@@ -9,8 +8,6 @@ from selenium.webdriver import FirefoxOptions
 from selenium.webdriver.common.keys import Keys
 from config import *
 from credentials import *
-
-geckodriver_autoinstaller.install()
 
 def run():
     initialTime = time.time()
